04c52940c31a79cb1fb1371a7f59bc8bbae6ab70
Cristian Nicoara <cristian.nicoara82@gmail.com>
2014-08-23 01:49:18 +0300

    Change name again. Add history for ip addresses

---
+++ check_ppp_interface.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/ruby 
+
+OKV=0
+WARNV=1
+CRITV=2
+UNKNV=3
+
+interfaceText = `ip a`
+interface = nil
+interfaceCount = 0
+ipAddress = nil
+lastValueFile = "/var/tmp/lastpppValue.txt"
+historyValueFile = "/var/tmp/historyPPPValue.txt"
+lastIpValue = nil
+ipChangeText = nil
+historyText = nil
+interfaceText.each_line do |line|
+	if line.include?("ppp") and line.include?("POINTOPOINT")
+		interface = line.split(":")[1].gsub(" ","")
+		interfaceCount += 1
+	end
+	if !interface.nil? and interface.include?("ppp") and line.include?("inet")
+		ipAddress = line.split(" ")[1]	
+	end
+end
+if interface.nil?
+	puts "No ppp interface found"
+	exit CRITV	
+end
+if ipAddress.nil? 
+	puts "ppp interface found=#{interface} but no IP found"
+	exit CRITV
+end
+if interfaceCount > 1
+	puts "Strange: more than one ppp interface found"
+	exit CRITV
+end
+if !File.exist?(lastValueFile)
+	fileHandle = File.new(lastValueFile, "w")
+	fileHandle.close
+end
+if !File.exist?(historyValueFile)
+	fileHandle = File.new(historyValueFile, "w")
+	fileHandle.close
+end
+fileHandle = File.open(lastValueFile, "r")
+fileText = fileHandle.read
+fileHandle.close
+if fileText
+	fileText = fileText.gsub(" ","").gsub("\n","")
+	lastIpValue = fileText
+#	puts "old=#{lastIpValue}|"
+#	puts "new=#{ipAddress}|"
+	if ipAddress.eql?(lastIpValue)
+		ipChangeText = "Same ip address."
+	else
+		ipChangeText = "IP address changed. old=#{lastIpValue}"
+		fileHandle = File.new(lastValueFile, "w")
+		fileHandle.puts(ipAddress)
+		fileHandle.close
+		fileHandle = File.new(historyValueFile, "a")
+		fileHandle.puts(ipAddress)	
+		fileHandle.close
+	end
+	fileHandle = File.new(historyValueFile, "r")
+	fileTempText = fileHandle.read
+	fileHandle.close
+	tempArray = fileTempText.split("\n")
+	historyText = "History, ip count #{tempArray.count}"
+end
+puts "Interface and ip found int=#{interface} ip=#{ipAddress}. #{ipChangeText} #{historyText}"
+exit OKV
