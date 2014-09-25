73f7a58d80452c88c4fbd9bade460a2a9461179f
Cristian Nicoara <cristian.nicoara@visma.com>
2014-09-25 14:03:32 +0300

    Return only non leaf hash keys

--- getAllKeysFromHash.rb
+++ getAllKeysFromHash.rb
@@ -2,14 +2,18 @@
 
 
 exhash = {
-	"gstuff_1" => "stuff1",
-	"gstuff_2" => {
-		"rstuff_1" => "stuff2",
+	"fstuff_1" => "stuff1",
+	"rstuff_2" => {
+		"fstuff_1" => "stuff2",
 		"rstuff_2" => {
-			"fstuff_1" => "stuff3"
+			"fstuff_1" => "stuff3",
+			"fstuff_2" => "stuff4",
+			"fstuff_3" => "stuff5"
 		}
 	}
 }
+
+## the purpose if to return only rstuff_* keys
 
 @all_keys = []
 
@@ -21,10 +25,10 @@ def get_hash_keys(small_hash)
 	small_keys = small_hash.keys
 	small_keys.each do |key|
 		puts "key=#{key}"
-		@all_keys << key	
 		temp_hash = small_hash[key]
 		puts "temp_hash=#{temp_hash} -- #{temp_hash.class.name}"
 		if temp_hash.class.name.eql? "Hash"
+			@all_keys << key
 			get_hash_keys(temp_hash)	
 		end
 	end
